e8a96980294e228c71c1ff449b911b491786c863
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-04-24 23:54:59 -0500

    quotes are now isolated

--- scraping_project.py
+++ scraping_project.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 res = requests.get("https://quotes.toscrape.com")
 soup = BeautifulSoup(res.text)
 
-print(soup.body)
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    print(quote.find(class_="text").get_text())
